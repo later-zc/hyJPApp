f507c502b52e9ab601f4b1739dcd67b4aafcd1ed
later_zc <2363442097@qq.com>
2023-02-01 11:22:01 +0800

    feat: update

--- src/components/grid-view/index.jsx
+++ src/components/grid-view/index.jsx
@@ -1,35 +1,34 @@
-import { memo } from "react";
-import { View } from "@tarojs/components";
-import Taro from "@tarojs/taro";
-import PropTypes from "prop-types";
-import styles from "./index.module.scss";
-import GridViewItem from "../grid-view-item";
+import { memo } from 'react'
+import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import PropTypes from 'prop-types'
+import styles from './index.module.scss'
+import GridViewItem from '../grid-view-item'
 
 const GridView = memo(function (props) {
-  const { goods } = props;
+  const { goods } = props
   function handleItemClick(goodInfo) {
-    console.log(goodInfo);
     Taro.navigateTo({
       url: '/pages/detail/index?link=' + goodInfo.goods_jump_url
     })
   }
   return (
-    <View className={styles["grid-view"]}>
+    <View className={styles['grid-view']}>
       {goods.map((goodInfo) => {
         return (
-          <View className={styles["item"]} key={goodInfo.goods_id}>
+          <View className={styles['item']} key={goodInfo.goods_id}>
             <GridViewItem
               goodInfo={goodInfo}
               onItemClick={handleItemClick}
             ></GridViewItem>
           </View>
-        );
+        )
       })}
     </View>
-  );
-});
-export default GridView;
+  )
+})
+export default GridView
 
 GridView.propTypes = {
-  goods: PropTypes.array,
-};
+  goods: PropTypes.array
+}
